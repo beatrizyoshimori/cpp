89d1c5c636f78876b47c7375760d7dc85bd28d59
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-11-03 21:27:50 -0300

    added add and search contact functions

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 19:18:18 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/11/01 22:03:46 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/11/03 21:09:46 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 PhoneBook::PhoneBook(void)
 {
 	std::cout << "Constructor called" << std::endl;
+	this->_index = 0;
+	this->numberOfContacts = 0;
 	return ;
 }
 
@@ -27,12 +29,48 @@ PhoneBook::~PhoneBook(void)
 
 void	PhoneBook::addContact(void)
 {
-	int	index;
-
-	index = this->_index;
 	if (this->_index > 7)
+	{
 		this->_index = 0;
-	this->_contacts[index].setFirstName();
-	this->_contacts[index].setLastName();
-	this->_contacts[index].setNickname();
+		this->numberOfContacts = 7;
+	}
+	this->_contacts[this->_index].setFirstName();
+	this->_contacts[this->_index].setLastName();
+	this->_contacts[this->_index].setNickname();
+	this->_contacts[this->_index].setPhoneNumber();
+	this->_contacts[this->_index].setDarkestSecret();
+	std::cout << GREEN << std::endl << "Added new contact!" << std::endl << RESET;
+	if (this->numberOfContacts < 7)
+		this->numberOfContacts = this->_index;
+	this->_index++;
+}
+
+void	PhoneBook::searchContact(void)
+{
+	std::cout << CLEAR_WINDOW << std::endl;
+	std::cout << BLUE << std::setw(27) << std::right << "PHONEBOOK" << RESET << std::endl;
+	std::cout << std::right << "|" << 
+	std::setw(10) << "Index" << "|" <<
+	std::setw(10) << "First name" << "|" <<
+	std::setw(10) << "Last name" << "|" <<
+	std::setw(10) << "Nickname" << "|" << std::endl;
+	for (int i = 0; i <= this->numberOfContacts; i++)
+	{
+		std::cout << std::right << "|" << 
+		std::setw(10) << i + 1;
+		this->_contacts[i].printPhonebookContact();
+	}
+	std::string	input;
+	int	contactIndex;
+	while (true)
+	{
+		std::cout << std::endl << "Choose an index: ";
+		getline(std::cin, input);
+		contactIndex = std::atoi(input.c_str());
+		if (input.length() != 1  || contactIndex < 1 || contactIndex > 8 || contactIndex > this->numberOfContacts + 1)
+			std::cout << RED << "Invalid index" << RESET << std::endl;
+		else
+			break ;
+	}
+	this->_contacts[contactIndex - 1].printContactInfo();
 }
--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 19:18:23 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/11/01 20:59:44 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/11/03 19:35:30 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 # define PHONEBOOK_HPP
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
 #include "Contact.hpp"
+
+# define RESET			"\033[0m"
+# define RED			"\033[31m"
+# define GREEN			"\033[32m"
+# define YELLOW			"\033[33m"
+# define BLUE			"\033[34m"
+# define MAGENTA		"\033[35m"
+# define CYAN			"\033[36m"
+# define CLEAR_WINDOW	"\033[2J\033[1;1H"
 
 class	PhoneBook
 {
@@ -22,7 +34,10 @@ class	PhoneBook
 		PhoneBook(void);
 		~PhoneBook(void);
 
+		int		numberOfContacts;
+
 		void	addContact();
+		void	searchContact();
 
 	private:
 		Contact	_contacts[8];
