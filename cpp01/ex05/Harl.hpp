d7e5ad8b4e7888972f5bf6413c0eb486060d80e6
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-11-25 21:59:29 -0300

    created Makefile and Harl class

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 18:59:01 by byoshimo          #+#    #+#             */
+/*   Updated: 2023/11/25 19:06:15 by byoshimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+
+	public:
+		Harl( void );
+		~Harl( void );
+
+		void	complain( std::string level );
+};
+
+#endif
