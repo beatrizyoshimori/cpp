713bf820f145d3fe27f6994243a9a954aacd4a9a
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-12-04 20:01:00 -0300

    added test

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 19:53:10 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/12/04 19:53:12 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/12/04 19:59:42 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,12 @@ int	main(void)
 	b.takeDamage(a.getAttackDamage());
 	b.attack("D");
 	d.takeDamage(b.getAttackDamage());
+	c.attack("A");
+	a.takeDamage(c.getAttackDamage());
+	c.attack("D");
+	d.takeDamage(c.getAttackDamage());
+	d.attack("C");
+	c.takeDamage(d.getAttackDamage());
 	d.highFivesGuys();
 	b.beRepaired(1);
 	c.guardGate();
@@ -37,4 +43,5 @@ int	main(void)
 	a.getStatus();
 	b.getStatus();
 	c.getStatus();
+	d.getStatus();
 }
