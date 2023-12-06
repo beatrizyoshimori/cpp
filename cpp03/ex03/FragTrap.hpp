75551d976f49a29c34b19c94dd56652eff61ca97
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-12-05 22:22:18 -0300

    added default constructor

--- cpp03/ex03/FragTrap.hpp
+++ cpp03/ex03/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 19:16:57 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/12/04 22:02:23 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/12/05 22:12:28 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,21 @@
 
 class FragTrap : public virtual ClapTrap
 {
-	private:
+	protected:
+		std::string		type;
+		const static int HIT_POINTS = 100;
+		const static int ENERGY_POINTS = 100;
+		const static int ATTACK_DAMAGE = 30;
 
 	public:
+		FragTrap(void);
 		FragTrap(std::string name);
 		FragTrap(const FragTrap &obj);
 		FragTrap& operator=(const FragTrap &obj);
 		~FragTrap(void);
 
 		void	attack(const std::string& target);
-		void	takeDamage(unsigned int amount);
+		// void	takeDamage(unsigned int amount);
 		void	highFivesGuys(void);
 };
 
