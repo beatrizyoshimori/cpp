6bbe16c4483044e743521cef87aa6e267fd4ade0
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-01-31 21:51:10 -0300

    test

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 11:55:11 by byoshimo          #+#    #+#             */
-/*   Updated: 2024/01/15 21:50:41 by byoshimo         ###   ########.fr       */
+/*   Updated: 2024/01/31 21:50:43 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,32 @@ int main()
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
+
+	std::cout << "-----------" << std::endl << std::endl;
+	// MateriaSource s;
+	// s.learnMateria(new Ice());
+	// s.learnMateria(new Cure());
+	// std::cout << s.getAMateria(0)->getType() << std::endl << s.getAMateria(1)->getType() << std::endl;
+	Character a("a");
+	tmp = src->createMateria("ice");
+	a.equip(tmp);
+	Character b = a;
+	std::cout << "1" << a.getAMateria(0)->getType() << std::endl;
+	std::cout << "2" << b.getAMateria(0)->getType() << std::endl;
+	// a.unequip(0);
+	tmp = a.getAMateria(0);
+	a.unequip(0);
+	delete tmp;
+	tmp = src->createMateria("cure");
+	a.equip(tmp);
+	std::cout << "3" << a.getAMateria(0)->getType() << std::endl;
+	std::cout << "4" << b.getAMateria(0)->getType() << std::endl;
+	// std::cout << "4" << b.getAMateria(1)->getType() << std::endl;
+	// std::cout << a.getAMateria(0)->getType() << std::endl;
+	// Character b;
+	// b = a;
+	// std::cout << b.getAMateria(0)->getType() << std::endl;
+	// std::cout << "-----------" << std::endl << std::endl;
 	delete bob;
 	delete me;
 	delete src;
