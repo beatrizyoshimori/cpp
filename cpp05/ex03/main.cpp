c6bf53e20120d80a04b27dc7e4927d29688ffcf5
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-02-16 21:58:15 -0300

    added try catch to tests

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 20:38:45 by byoshimo          #+#    #+#             */
-/*   Updated: 2024/01/17 21:21:19 by byoshimo         ###   ########.fr       */
+/*   Updated: 2024/02/16 21:55:05 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,14 @@ int	main(void)
 		std::cout << std::endl << "---------- SUBJECT TEST ----------" << std::endl << std::endl;
 		Intern	someRandomIntern;
 		AForm*	rrf;
-		rrf = someRandomIntern.makeForm("Robotomy Request Form", "Bender");
+		try
+		{
+			rrf = someRandomIntern.makeForm("Robotomy Request Form", "Bender");
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 		delete rrf;
 	}
 	{
@@ -32,9 +39,30 @@ int	main(void)
 		AForm	*b;
 		AForm	*c;
 
-		a = intern.makeForm("", "");
-		b = intern.makeForm("invalid", "");
-		c = intern.makeForm("robotomy request", "");
+		try
+		{
+			a = intern.makeForm("", "");
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			b = intern.makeForm("invalid", "");
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			c = intern.makeForm("robotomy request", "");
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
 	{
 		std::cout << std::endl << "---------- INTERN CLASS TEST ----------" << std::endl << std::endl;
@@ -52,18 +80,64 @@ int	main(void)
 		std::cout << *b << std::endl;
 		std::cout << *c << std::endl << std::endl;
 
-		bure.signForm(*a);
-		bure.signForm(*b);
-		bure.signForm(*c);
+		try
+		{
+			bure.signForm(*a);
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bure.signForm(*b);
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bure.signForm(*c);
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 
 		std::cout << std::endl << *a << std::endl;
 		std::cout << *b << std::endl;
 		std::cout << *c << std::endl << std::endl;
 
-		bure.executeForm(*a);
-		bure.executeForm(*b);
-		bure.executeForm(*c);
+		try
+		{
+			bure.executeForm(*a);
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bure.executeForm(*b);
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bure.signForm(*c);
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 		std::cout << std::endl;
+
+		delete a;
+		delete b;
+		delete c;
 	}
 	return (0);
 }
