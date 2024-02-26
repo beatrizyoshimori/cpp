867ee837547eb4ad5e084351fbdd359455189f57
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-02-26 19:35:57 -0300

    test

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 18:50:24 by byoshimo          #+#    #+#             */
-/*   Updated: 2024/02/26 18:57:04 by byoshimo         ###   ########.fr       */
+/*   Updated: 2024/02/26 19:35:14 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,16 @@ int	main(void)
 	float		b[] = {2.5, 3.7, 99.5, 10.3};
 	std::string	c[] = {"oi", "tudo", "bem", "?"};
 	const int	d[] = {9, 8, 7, 6};
+	char		*e = NULL;
 
 	std::cout << "TEST INT" << std::endl;
-	::iter<int>(a, 4, ::print);
+	::iter(a, 4, ::print);
 	std::cout << std::endl << "TEST FLOAT" << std::endl;
-	::iter<float>(b, 4, ::print);
+	::iter(b, 4, ::print);
 	std::cout << std::endl << "TEST STRING" << std::endl;
-	::iter<std::string>(c, 4, ::print);
+	::iter(c, 4, ::print);
 	std::cout << std::endl << "TEST CONST INT" << std::endl;
-	::iter<int>(d, 4, ::print);
+	::iter(d, 4, ::print);
+	std::cout << std::endl << "TEST NULL" << std::endl;
+	::iter(e, 1, ::print);
 }
