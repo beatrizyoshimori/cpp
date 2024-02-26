4b6451df53af41091353d9ce5792da3ec0fb9d08
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-02-26 18:10:47 -0300

    added header

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -1,10 +1,22 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 18:09:46 by byoshimo          #+#    #+#             */
+/*   Updated: 2024/02/26 18:09:49 by byoshimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
 
 #include "Data.hpp"
 #include <iostream>
+#include <stdint.h>
+#include <cstdlib>
 
 class Serializer
 {
